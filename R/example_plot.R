62d99cffda8cef9455e3e5e6a5a83e499287aee2
Matt Cowgill <mattcowgill@gmail.com>
2021-04-20 21:50:38 +1000

    tidy up namespace

--- R/example_plot.R
+++ R/example_plot.R
@@ -1,11 +1,12 @@
 #' @import ggplot2
+#' @importFrom rlang .data .env
 
 example_plot <- function(data = ggplot2::economics,
                          title = "A title",
                          subtitle = "A very long subtitle lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
                          caption = "A caption") {
   data %>%
-    ggplot(aes(x = date, y = unemploy)) +
+    ggplot(aes(x = .data$date, y = .data$unemploy)) +
     geom_line() +
     labs(
       title = title,
