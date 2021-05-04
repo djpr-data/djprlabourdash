70a736f1b6164a7b67010444b92e6600e7b20e07
Matt Cowgill <mattcowgill@gmail.com>
2021-05-04 17:11:28 +1000

    working checkbox

--- R/example_plot.R
+++ R/example_plot.R
@@ -1,14 +1,18 @@
 #' @import ggplot2
 #' @importFrom rlang .data .env
 
-example_plot <- function(data = ggplot2::economics_long %>%
-                           dplyr::filter(.data$variable == "unemploy") %>%
-                           dplyr::rename(series = .data$variable),
+example_plot <- function(data,
                          title = "A title",
                          subtitle = "A very long subtitle lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
                          caption = "A caption") {
 
-  djprshiny::djpr_ts_linechart(df = data) +
+  df <- data %>%
+    dplyr::filter(.data$series_id %in%
+                    find_lfs_series("unemployment rate",
+                                    state = "victoria",
+                                    sex = c("males", "females")))
+
+  djprshiny::djpr_ts_linechart(df) +
     labs(title = title,
          subtitle = subtitle,
          caption = caption)
--- R/load_dash_data.R
+++ R/load_dash_data.R
@@ -8,6 +8,18 @@ load_and_hide <- function() {
   df
 }
 
+
 load_dash_data <- function() {
-  djprdashdata::download_abs_ts("abs-lfs")
+  crosstabs <- djprdashdata::lfs_lookup %>%
+    dplyr::select(-dplyr::one_of(c("cat_no",
+                                   "table_no",
+                                   "series",
+                                   "series_type")
+                                 )
+                  )
+
+  djprdashdata::download_abs_ts("abs-lfs") %>%
+    tidyr::unnest(cols = data) %>%
+    dplyr::left_join(crosstabs,
+                     by = "series_id")
 }
