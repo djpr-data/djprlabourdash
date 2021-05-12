d8c6333af146402f2ff4c93e0e05a8b60461b793
annett-djpr <annett.finger@ecodev.vic.gov.au>
2021-05-12 15:31:42 +1000

    Added IDs in data in function header and subtitles and captions in groups.

--- R/groups.R
+++ R/groups.R
@@ -12,25 +12,37 @@
 #' ids <- c("A84423354L",
 #'          "A84423242V",
 #'          "A84423466F")
+#'
+#' for viz_gr_yth_lfpart_line:
+#' ids <- will need smoothing and extracting data from LM1.
+#'
+#' for viz_gr_yth_emp_line:
+#'
 
 
-Viz_gr_gen_partrate_line <- function(data, title = "") {
+Viz_gr_gen_partrate_line <- function(data = filter_dash_data(c("A84423355R",
+                                                               "A84423243W",
+                                                               "A84423467J"),
+                                                               df = dash_data),
+                                                               title = "") {
 
-  df <- data %>%
-    dplyr::group_by(series)
-
-  df %>%
+  data %>%
     djpr_ts_linechart() +
-    labs(title = title)
+    labs(title = title,
+         subtitle = "Participation rate by sex, Victoria",
+         caption = "Source: ABS Labour Force. Note: seasonally adjusted data.")
 }
 
-viz_gr_gen_unemp_line <- function(data, title = "") {
+viz_gr_gen_unemp_line <- function(data = filter_dash_data(c("A84423354L",
+                                                            "A84423242V",
+                                                            "A84423466F"),
+                                                            df = dash_data,
+                                                            title = "")) {
 
-  df <- data %>%
-    dplyr::group_by(series)
-
-  df %>%
+  data %>%
     djpr_ts_linechart() +
-    labs(title = title)
+    labs(title = title,
+         subtitle = "Unemployment by sex, Victoria",
+         caption = "Source: ABS Labour Force. Note: seasonally adjusted data.")
 }
 
