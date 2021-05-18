94d2900a2bfea52d88c243290a522772e057f188
Matt Cowgill <mattcowgill@gmail.com>
2021-05-18 16:58:33 +1000

    logo at right - drop for now

--- R/labour_ui.R
+++ R/labour_ui.R
@@ -1,6 +1,10 @@
 labour_ui <- function(...) {
   djprshiny::djpr_page(
     title = "DJPR Jobs Dashboard",
+#     tags$script(HTML("var header = $('.navbar > .container-fluid');
+# header.append('<div style=\"float:right\">SPP Data + Analytics</div>');
+#     console.log(header)")
+#     ),
     page_overview(),
     page_indicators(),
     page_groups(),
