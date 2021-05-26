75782ca62742f25b918ab20b80f481e00792ebd6
Matt Cowgill <mattcowgill@gmail.com>
2021-05-26 10:18:55 +1000

    throw warning when requested series ID(s) not found

--- R/filter_dash_data.R
+++ R/filter_dash_data.R
@@ -1,11 +1,26 @@
 #' @import dplyr
-
+#' Convenience function to extract specified ABS series IDs from `dash_data`
+#'
+#' @param series_ids Vector of series ID(s)
+#' @param df Data frame, expected to be the df returned by `load_dash_data()`
+#' @return An unnested tbl_df containing only the specified series ID(s)
+#' @examples
+#' dash_data <- load_dash_data()
+#' filter_dash_data("A84423354L")
 
 filter_dash_data <- function(series_ids, df = dash_data) {
-  df %>%
+  out <- df %>%
     dplyr::filter(.data$series_id %in% series_ids) %>%
     tidyr::unnest(cols = .data$data) %>%
     dplyr::mutate_if(is.factor, as.character)
+
+  all_present <- all(.env$series_ids %in% out$series_id)
+
+  if (!all_present) {
+    warning("Not all series IDs could be found")
+  }
+
+  out
 }
 
 
