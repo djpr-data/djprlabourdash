8a75e849afb3d3c6e3c8fa5174083ee3049baaaa
Matt Cowgill <mattcowgill@gmail.com>
2021-06-15 22:03:12 +1000

    get logo from djprshiny

--- R/labour_ui.R
+++ R/labour_ui.R
@@ -1,8 +1,6 @@
 labour_ui <- function(...) {
-  addResourcePath("www", "inst/www")
   djprshiny::djpr_page(
     title = "DJPR Jobs Dashboard",
-    logo = "www/spp_data_logo.png",
     page_overview(),
     page_indicators(),
     page_inclusion(),
