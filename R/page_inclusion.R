3631cb6700a4afddede7c3bd0f9d99dcc703c592
Matt Cowgill <mattcowgill@gmail.com>
2021-06-22 12:10:37 +1000

    remove intro text on pages

--- R/page_inclusion.R
+++ R/page_inclusion.R
@@ -2,16 +2,16 @@ page_inclusion <- function(...) {
   djpr_tab_panel(
     title = "Inclusion",
     h1("Key groups"),
-    tagList(
-      "This page contains information about the labour force status of key groups of ",
-      "Victorians, such as women, and young people. ",
-      htmltools::tags$b("More information will be included with future releases. "),
-      "For more information about overall labour force indicators ",
-      "see the ",
-      actionLink("link_indicators", "indicators page"),
-      ". For information about how employment and unemployment varies across Victoria, see the ",
-      actionLink("link_regions", "regions page"), "."
-    ),
+    # tagList(
+    #   "This page contains information about the labour force status of key groups of ",
+    #   "Victorians, such as women, and young people. ",
+    #   htmltools::tags$b("More information will be included with future releases. "),
+    #   "For more information about overall labour force indicators ",
+    #   "see the ",
+    #   actionLink("link_indicators", "indicators page"),
+    #   ". For information about how employment and unemployment varies across Victoria, see the ",
+    #   actionLink("link_regions", "regions page"), "."
+    # ),
     br(),
     h2("Women and men"),
     djpr_plot_ui("gr_gen_emp_bar"),
--- R/page_overview.R
+++ R/page_overview.R
@@ -19,7 +19,7 @@ page_overview <- function(...) {
       centred_row(
         HTML("Note: this dashboard is a prototype in active development. The content is not yet complete. <a href='mailto:matt.cowgill@ecodev.vic.gov.au?subject=DJPR Jobs Dashboard'>Feedback is welcome</a>."),
       ),
-      centred_row(h3("Overview")),
+      # centred_row(h3("Overview")),
       centred_row(
         tagList(
           reactable::reactableOutput("main_table") %>%
