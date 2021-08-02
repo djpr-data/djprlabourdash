86e05be740138b8fa317dcf58bba2fcca8de8f61
Matt Cowgill <mattcowgill@gmail.com>
2021-08-02 15:19:46 +1000

    add viz_reg_unemp_emppop_partrate_multiline to dashboard

--- R/page_regions.R
+++ R/page_regions.R
@@ -39,7 +39,17 @@ page_regions <- function(...) {
     ),
     djpr_plot_caption("Source: ABS Labour Force, Detailed (monthly). Note: data is not seasonally adjusted; smoothed using a 3 month rolling average."),
     br(),
-    djpr_plot_ui("reg_unemprate_multiline",
+    br(),
+    selectInput("lf_status_multiline",
+                label = "Choose an indicator",
+                choices = c(
+                  "Unemployment rate" = "unemp_rate",
+                  "Participation rate" = "part_rate",
+                  "Employment to population ratio" = "emp_pop"
+                ),
+                selected = "unemp_rate"
+    ),
+    djpr_plot_ui("reg_unemp_emppop_partrate_multiline",
       height = "500px"
     ),
     djpr_plot_ui("reg_unemprate_dispersion"),
