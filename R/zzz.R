a4059be365a7b2dd131c199435e5563093547d2a
Matt Cowgill <mattcowgill@gmail.com>
2021-08-11 16:49:52 +1000

    speed tweaks

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,7 @@
     )
   )
 
-  if (suppressWarnings(require(memoise))) {
+  if (requireNamespace("memoise", quietly = TRUE)) {
     make_table_mem <<- memoise::memoise(make_table)
   } else {
     make_table_mem <<- make_table
