2cf8df246cb7c45b4ef4dc35fd2670e674313c71
Matt Cowgill <mattcowgill@gmail.com>
2021-08-11 16:03:36 +1000

    memoise make_table

--- R/zzz.R
+++ R/zzz.R
@@ -4,4 +4,10 @@
       dir = file.path(dirname(tempdir()), "djpr-jobs-dash-cache")
     )
   )
+
+  if (suppressWarnings(require(memoise))) {
+    make_table_mem <<- memoise::memoise(make_table)
+  } else {
+    make_table_mem <<- make_table
+  }
 }
