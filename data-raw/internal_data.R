3c599ed8f48a37dcee0f2dac41c619ea30c35278
Matt Cowgill <mattcowgill@gmail.com>
2021-08-14 14:04:35 +1000

    load dash_data as internal data

--- data-raw/internal_data.R
+++ data-raw/internal_data.R
@@ -1,7 +1,25 @@
 ## Create internal data
+pkgload::load_all()
 
 # Load absmapsdata object(s) to avoid dependency
 sa42016 <- absmapsdata::sa42016
 
+# Load data from djprdashdata if it has been updated
+temp_loc <- tempfile(fileext = ".rds")
+download.file(url = "https://github.com/djpr-data/djprdashdata/blob/main/data-raw/last_updated.rds?raw=true",
+              destfile = temp_loc)
 
-usethis::use_data(sa42016, internal = TRUE, overwrite = TRUE)
+remote_updated <- readRDS(temp_loc)
+remote_updated <- as.POSIXct(remote_updated)
+
+dash_data_updated <- dash_data_updated
+
+if (dash_data_updated < remote_updated) {
+  dash_data <- load_dash_data()
+  dash_data_updated <- remote_updated
+}
+
+usethis::use_data(sa42016,
+                  dash_data_updated,
+                  dash_data,
+                  internal = TRUE, overwrite = TRUE)
