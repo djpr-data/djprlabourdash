a3f8417407eebd76a35049c2f69a7b86aeef5cd2
Matt Cowgill <mattcowgill@gmail.com>
2021-09-09 10:22:27 +1000

    tweak test

--- tests/testthat/test-all-viz.R
+++ tests/testthat/test-all-viz.R
@@ -1,7 +1,12 @@
+
 test_that("all viz_ functions at least produce a plot", {
 
-  for (f in ls(getNamespace("djprlabourdash"), pattern = "viz_")) {
-    testthat::expect_s3_class(get(f)(), "gg")
+  plot_funcs <- ls(getNamespace("djprlabourdash"), pattern = "viz_")
+
+  plots <- lapply(plot_funcs, function(x) get(x)())
+
+  for (p in plots) {
+    testthat::expect_s3_class(p, "gg")
   }
 
 })
