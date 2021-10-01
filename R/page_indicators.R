780845b01744ce3d82492802ce04dd1dfe7c51df
annett-djpr <annett.finger@ecodev.vic.gov.au>
2021-10-01 10:30:31 +1000

    Not focus box, but side-by-side for the employment graphs.

--- R/page_indicators.R
+++ R/page_indicators.R
@@ -8,15 +8,14 @@ page_indicators <- function(...) {
     uiOutput("ind_emp_table") %>%
       djpr_with_spinner(hide.ui = TRUE),
     br(),
-    focus_box(
-        fluidRow(
-          column(6, djpr_plot_ui("ind_emppop_state_slope")),
-          column(6, djpr_plot_ui("ind_empgro_line"))
-        ),
-        fluidRow(
-          column(6, djpr_plot_ui("ind_gen_full_part_line")),
-          column(6, djpr_plot_ui("ind_emp_sincecovid_line"))
-        )
+      fluidRow(
+        column(6, djpr_plot_ui("ind_emppop_state_slope")),
+        column(6, djpr_plot_ui("ind_empgro_line"))
+      ),
+    br(),
+      fluidRow(
+        column(6, djpr_plot_ui("ind_gen_full_part_line")),
+        column(6, djpr_plot_ui("ind_emp_sincecovid_line"))
     ),
     h2(br(), "Unemployment & underemployment"),
     uiOutput(
