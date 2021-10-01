639a71183c90e792f47d79ef486445fc136f10e1
annett-djpr <annett.finger@ecodev.vic.gov.au>
2021-10-01 16:46:16 +1000

    added text to top of lt_unemp page.

--- R/page_ltunemp.R
+++ R/page_ltunemp.R
@@ -2,6 +2,12 @@ page_ltunemp <- function(...) {
   djpr_tab_panel(
     title = "Long-term unemployed",
     br(),
+    paste0("Long-term unemployment is defined as a duration of unemployment of 12 months or more, "),
+    paste0("calculated from the time a person either last worked in any job for two weeks or more, "),
+    paste0("or began actively looking for work (whichever is the more recent). "),
+    paste0("Measuring long-term unemployment is important as it impacts on communities both socially "),
+    paste0(" and economically. Compared to short-term unemployed people, those unemployed for longer "),
+    paste0("periods of time can experience higher levels of competition, decreased confidence and motivation."),
     djpr_plot_ui("gr_ltunemp_line"),
     djpr_plot_ui("gr_ltunvic_bar",
                  interactive = FALSE
