fa39248d2de15196cf0ff7d91cb45886a8cdadae
Matt Cowgill <mattcowgill@gmail.com>
2021-10-14 16:51:24 +1100

    stupid onedrive

--- tests/testthat/test-get_summ 2.R
+++
@@ -1,9 +0,0 @@
-test_that("get_summ() returns expected output", {
-  ts_summ <- dash_data %>%
-    djprshiny::ts_summarise()
-
-  expect_equal(
-    get_summ("A84601638A", series, df = ts_summ),
-    "> Victoria ;  Accommodation and Food Services ;  Employed total ;"
-  )
-})
