570d094a589a49a8a156eaf8ca1b352a58fae1f1
Annett Finger (DJPR) <annett.finger@ecodev.vic.gov.au>
2021-11-16 16:39:59 +1100

    removed one extra blank space.

--- R/page_ltunemp.R
+++ R/page_ltunemp.R
@@ -6,7 +6,7 @@ page_ltunemp <- function(...) {
     paste0("calculated from the time a person either last worked in any job for two weeks or more, "),
     paste0("or began actively looking for work (whichever is the more recent). "),
     paste0("Measuring long-term unemployment is important as it impacts on communities both socially "),
-    paste0(" and economically. Compared to short-term unemployed people, those unemployed for longer "),
+    paste0("and economically. Compared to short-term unemployed people, those unemployed for longer "),
     paste0("periods of time can experience higher levels of competition, decreased confidence and motivation."),
     djpr_plot_ui("gr_ltunemp_line"),
     djpr_plot_ui("gr_ltunvic_bar",
