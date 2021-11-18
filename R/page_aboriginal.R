1443b8320648c1e1e695813af812dd895b0cfac7
Annett Finger (DJPR) <annett.finger@ecodev.vic.gov.au>
2021-11-18 16:19:15 +1100

    Chnaged title to remove Torre Strait Islanders.

--- R/page_aboriginal.R
+++ R/page_aboriginal.R
@@ -1,9 +1,8 @@
 page_aboriginal <- function(...) {
   djpr_tab_panel(
-    title = "Aboriginal and Torre Strait Islander Peoples",
+    title = "Aboriginal Victorians",
     br(),
-    paste0("This section explores the labour force status for Aboriginal "),
-    paste0("and Torre Strait Islander Peoples"),
+    paste0("This section explores the labour force status for Aboriginal Victorians"),
     br(),
     br(),
     br(),
