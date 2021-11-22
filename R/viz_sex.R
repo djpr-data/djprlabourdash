301a98139eab76338bcc058fce0d9bb0301c81cc
Emma Seyoum-Tegegn (DJPR) <emma.seyoum-tegegn@ecodev.vic.gov.au>
2021-11-23 09:18:03 +1100

    viz_gr_female_jobactive_bar function has been created

--- R/viz_sex.R
+++ R/viz_sex.R
@@ -696,3 +696,107 @@ df = dash_data
       caption = caption_jobactive()
     )
 }
+
+viz_gr_female_jobactive_bar <- function(data = filter_dash_data(c(
+  "jobactive_female_ballarat",
+  "jobactive_female_bendigo",
+  "jobactive_female_barwon",
+  "jobactive_female_gippsland",
+  "jobactive_female_goulburn/murray",
+  "jobactive_female_inner metropolitan melbourne",
+  "jobactive_female_north eastern melbourne",
+  "jobactive_female_north western melbourne",
+  "jobactive_female_south coast of victoria",
+  "jobactive_female_south eastern melbourne and peninsula",
+  "jobactive_female_north western melbourne",
+  "jobactive_female_wimmera mallee"
+),
+df = dash_data
+)) {
+  df <- data %>%
+    dplyr::select(
+      .data$date, .data$series,
+      .data$unit, .data$value
+    ) %>%
+    dplyr::mutate(
+      split_series = stringr::str_split_fixed(.data$series,
+                                              pattern = " ; ",
+                                              n = 3
+      ),
+      jobactive = .data$split_series[, 1],
+      indicator = .data$split_series[, 2],
+      region = .data$split_series[, 3]
+    ) %>%
+    dplyr::select(-.data$split_series, -.data$series, -.data$jobactive, -.data$indicator) %>%
+    dplyr::mutate(
+      value = .data$value * 1000
+    )
+
+  # value = scales::comma(.data$value * 1000),
+  high_low <- df %>%
+    dplyr::group_by(.data$date) %>%
+    summarise(
+      min_region = .data$region[.data$value == min(.data$value)],
+      min_caseload = .data$value[.data$value == min(.data$value)],
+      max_region = .data$region[.data$value == max(.data$value)],
+      max_caseload = .data$value[.data$value == max(.data$value)],
+      date = max(.data$date)
+    )
+
+  title <- paste0(
+    " Across Victoria jobactive caseload ranged from ",
+    round2(high_low$min_caseload, 1),
+    " for female in ",
+    high_low$min_region,
+    " to ",
+    round2(high_low$max_caseload, 1),
+    " for female in ",
+    high_low$ max_region,
+    " as at ",
+    format(high_low$date, "%B %Y")
+  )
+
+  # reduce to only latest month
+  df <- df %>%
+    dplyr::group_by(.data$region, ) %>%
+    dplyr::filter(.data$date == max(.data$date)) %>%
+    dplyr::ungroup()
+
+
+
+  # draw bar chart for all employment regions
+  df %>%
+    ggplot(aes(
+      x = stats::reorder(.data$region, .data$value),
+      y = .data$value
+    )) +
+    geom_col(
+      col = "grey85",
+      aes(fill = -.data$value)
+    ) +
+    geom_text(
+      nudge_y = 0.1,
+      aes(label = paste0(round2(.data$value, 0))),
+      colour = "black",
+      hjust = 0,
+      size = 12 / .pt
+    ) +
+    coord_flip(clip = "off") +
+    scale_fill_distiller(palette = "Blues") +
+    scale_y_continuous(expand = expansion(mult = c(0, 0.15))) +
+    djprtheme::theme_djpr(flipped = TRUE) +
+    theme(
+      axis.title.x = element_blank(),
+      panel.grid = element_blank(),
+      axis.text.y = element_text(size = 12),
+      axis.text.x = element_blank()
+    ) +
+    labs(
+      title = title,
+      subtitle = paste0(
+        "Victorians jobactive caseload by region, ",
+        format(max(data$date), "%B %Y")
+      ),
+      caption = caption_jobactive()
+    )
+}
