722cbedd073d6deeaf30a7ad9ec26c2aaea1ea16
Emma Seyoum-Tegegn (DJPR) <emma.seyoum-tegegn@ecodev.vic.gov.au>
2021-11-23 16:42:17 +1100

    title updated to the lates date

--- R/viz_disability.R
+++ R/viz_disability.R
@@ -149,6 +149,7 @@ df = dash_data
 
   # value = scales::comma(.data$value * 1000),
   high_low <- df %>%
+    group_by(.data$date) %>%
     summarise(
       min_region = .data$region[.data$value == min(.data$value)],
       min_caseload = .data$value[.data$value == min(.data$value)],
