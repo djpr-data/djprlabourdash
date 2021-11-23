19fd5dfc00149823155596fc80b527a5327d41da
Emma Seyoum-Tegegn (DJPR) <emma.seyoum-tegegn@ecodev.vic.gov.au>
2021-11-23 15:03:25 +1100

    Issue with title fixed

--- R/viz_age.R
+++ R/viz_age.R
@@ -1676,6 +1676,11 @@ df = dash_data
     format(high_low$date, "%B %Y")
   )
 
+  df <- df %>%
+    dplyr::group_by(.data$region, ) %>%
+    dplyr::filter(.data$date == max(.data$date)) %>%
+    dplyr::ungroup()
+
   # draw bar chart for all employment regions
   df %>%
     ggplot(aes(
@@ -1760,10 +1765,16 @@ df = dash_data
       date = max(.data$date)
     )
 
+  df <- df %>%
+    dplyr::group_by(.data$region, ) %>%
+    dplyr::filter(.data$date == max(.data$date)) %>%
+    dplyr::ungroup()
+
+
   title <- paste0(
     " Across Victoria youth (15-24) jobactive caseload ranged from ",
     round2(high_low$min_caseload, 1),
-    " for mature age (50+) in ",
+    " for youth (15-24) in ",
     high_low$min_region,
     " to ",
     round2(high_low$max_caseload, 1),
@@ -1772,6 +1783,8 @@ df = dash_data
     " as at ",
     format(high_low$date, "%B %Y")
   )
+
+
 
   # draw bar chart for all employment regions
   df %>%
