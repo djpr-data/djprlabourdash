26e8ddc271c4e43c18ca23bc53a72e53c6414d2b
Matt Cowgill <mattcowgill@gmail.com>
2021-11-26 15:41:50 +1100

    refugee bar

--- R/viz_migration.R
+++ R/viz_migration.R
@@ -156,13 +156,13 @@ viz_gr_refugee_jobactive_bar <- function(data = filter_dash_data(c(
     )
 
   title <- paste0(
-    " Across Victoria jobactive caseload ranged from ",
+    "The number of refugees receiving jobactive assistance ranged from ",
     round2(high_low$min_caseload, 1),
-    " for refugee in ",
+    " refugees in ",
     high_low$min_region,
     " to ",
     round2(high_low$max_caseload, 1),
-    " for refugee in ",
+    " refugees in ",
     high_low$ max_region,
     " as at ",
     format(high_low$date, "%B %Y")
@@ -172,6 +172,11 @@ viz_gr_refugee_jobactive_bar <- function(data = filter_dash_data(c(
 
   # draw bar chart for all employment regions
   df %>%
+    dplyr::mutate(
+      region = gsub("South Eastern Melbourne",
+                    "SE Melbourne",
+                    .data$region)
+    ) %>%
     ggplot(aes(
       x = stats::reorder(.data$region, .data$value),
       y = .data$value
@@ -182,7 +187,11 @@ viz_gr_refugee_jobactive_bar <- function(data = filter_dash_data(c(
     ) +
     geom_text(
       nudge_y = 0.1,
-      aes(label = paste0(round2(.data$value, 1))),
+      aes(label = paste0(
+        scales::comma(round2(.data$value, 1),
+                      accuracy = 1)
+        )
+          ),
       colour = "black",
       hjust = 0,
       size = 12 / .pt
