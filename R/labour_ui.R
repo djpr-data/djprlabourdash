47ddd9c5cee158ff314ea4035da9ed7d5594e512
Duy Nguyen <83742038+duyjpr@users.noreply.github.com>
2022-04-20 10:38:22 +1000

    Add favicon.ico
    The location of the www/ folder works when deployed to shinyapps

--- R/labour_ui.R
+++ R/labour_ui.R
@@ -1,6 +1,9 @@
 labour_ui <- function(...) {
   djprshiny::djpr_page(
     title = shiny::HTML("DJPR Jobs<br>Dashboard"),
+    htmltools::tags$head(
+      htmltools::tags$link(rel="icon", href="favicon.ico")
+    ),
     page_overview(),
     page_indicators(),
     navbarMenu(
