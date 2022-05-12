6dccdb9408cb35b159474c81eb886d50684da369
Annett Finger <annett.finger@ecodev.vic.gov.au>
2022-05-12 16:43:43 +1000

    Created labour_server.R and shuffled bits in there.

--- R/labour_app.R
+++ R/labour_app.R
@@ -6,56 +6,18 @@
 #' @import djprtheme
 #' @importFrom rlang .data .env
 
-labour_server <- function(input, output, session) {
-  # Load data and create persistent objects ----
-
-  Sys.setenv("R_DJPRLABOURDASH_TABLEDEST" = "dashboard")
-  dash_data <<- get_dash_data()
-  dash_data_updated <<- attr(dash_data, "date_updated")
-  if (shiny::isRunning()) {
-    shinyjs::hide("loading_page")
-    shinyjs::show("main_content")
-  }
-
-  series_latestdates <- dash_data %>%
-    dplyr::group_by(.data$series_id) %>%
-    dplyr::filter(.data$date == max(.data$date)) %>%
-    dplyr::pull(.data$date)
-
-  plt_change <- reactive(input$plt_change) %>%
-    debounce(2)
-
-  # Overview ------
-  # Overview: footnote and main table ----
-  # Indicators -----
-  # Indicators: Employment ----
-  # Sex -----
-  # Age ----
-  # Long-term unemployment ------
-  # Aboriginal ------
-  # Disability ------
-  # Migration ------
-  # Regions ------
-  # Victorian Regions -------
-  # Australian Regions -----------
-  # Industries ------
-  # Links to pages -----
-
-  app <- function(...) {
-    djprtheme::djpr_use_fonts()
-    flextable::set_flextable_defaults(
-      font.family = getOption("djprtheme.base_font_family")
+app <- function(...) {
+  djprtheme::djpr_use_fonts()
+  flextable::set_flextable_defaults(
+    font.family = getOption("djprtheme.base_font_family")
+    )
+  jobs_dash_cache <- cachem::cache_disk(
+    dir = file.path(".", "app-cache")
     )
 
-    jobs_dash_cache <- cachem::cache_disk(
-      dir = file.path(".", "app-cache")
-    )
+  shinyOptions(cache = jobs_dash_cache)
 
-    shinyOptions(
-      cache = jobs_dash_cache
-    )
-
-    shiny::shinyApp(labour_ui(), labour_server)
-  }
-
+  shiny::shinyApp(labour_ui(), labour_server)
 }
+
+
--- R/page_overview.R
+++ R/page_overview.R
@@ -43,28 +43,6 @@ page_overviewUI <- function(...) {
 
 page_overview <- function(input, output, session, plt_change) {
 
-  footnote <- reactive({
-    req(dash_data)
-    latest <- max(series_latestdates)
-    div(
-      shiny::HTML(
-        paste0(
-          "This dashboard is produced by the <b>Strategy and Priority ",
-          "Projects - Data + Analytics</b> team at the Victorian Department ",
-          "of Jobs, Precincts and Regions. The <b>latest data in this ",
-          "dashboard is for ",
-          format(latest, "%B %Y"),
-          '</b>. Please <a href="mailto:spp-data@ecodev.vic.gov.au?subject=DJPR Jobs Dashboard">email us</a> with any comments or feedback.'
-        )
-      ),
-      style = "color: #828282; font-size: 0.75rem"
-    )
-  })
-
-  output$aboriginal_footnote <- output$vicregions_footnote <- output$disability_footnote <- output$migration_footnote <- output$overview_footnote <- output$indicators_footnote <- output$inclusion_footnote <- output$regions_footnote <- output$industries_footnote <- renderUI({
-    footnote()
-  })
-
   output$main_table <- renderUI({
     req(dash_data)
     table_overview() %>%
