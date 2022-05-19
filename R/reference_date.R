407ef915cd5e2f86f30e5ea932728350f5fc82a0
Matt Johnson <mrjoh3@gmail.com>
2022-05-19 17:03:37 +1000

    match ref date on webpage, modify preamble based on detailed (y/n)

--- R/reference_date.R
+++ R/reference_date.R
@@ -1,18 +1,28 @@
 
-#' Get Labour Force Reference Dates
+#' @title Labour Force Reference Dates
+#' @description Get Labour Force reference dates and generate email preamble
 #'
-#' @import rvest
-#' @import httr
+#' @param detailed logical standard or detailed Labour Force release
+#'
+#' @importFrom rvest read_html html_elements html_table
+#' @importFrom purrr keep flatten_df
+#' @importFrom stringr str_replace
 #' @import lubridate
 #'
 #' @return list
 #' @export
 #'
 #' @examples
-reference_dates <- function() {
+reference_dates <- function(detailed = FALSE) {
   url <- "https://www.abs.gov.au/statistics/labour/employment-and-unemployment/labour-force-australia/latest-release#media-releases"
 
-  pub_dates <- rvest::read_html(url) |>
+  content <- rvest::read_html(url)
+
+  reference_period <- content |>
+    rvest::html_elements('.field--name-field-abs-reference-period .field__item') |>
+    rvest::html_text()
+
+  pub_dates <- content |>
     rvest::html_table() |>
     purrr::keep(~ "Publication" %in% colnames(.x)) |>
     purrr::flatten_df()
@@ -21,9 +31,10 @@ reference_dates <- function() {
     dplyr::filter(grepl(
       pattern = lubridate::month(Sys.Date() %m-% months(1),
         label = TRUE,
-        abbr = FALSE
+        abbr = TRUE
       ),
-      .data$Publication
+      .data$Publication,
+      ignore.case = TRUE
     )) |>
     dplyr::mutate(across(everything(), ~ stringr::str_replace(.x, " 2022", "")))
 
@@ -36,17 +47,17 @@ reference_dates <- function() {
     dplyr::pull(period)
 
   preamble <- paste0(
-    "The ABS released the latest detailed",
-    " Labour Force figures at 11:30am today. This data was ",
+    "The ABS released the latest ",
+    ifelse(detailed, 'detailed ', ''),
+    "Labour Force figures at 11:30am today. This data was ",
     "collected for the period ",
     pub_dates_text,
     glue::glue(" {lubridate::year(Sys.Date())}.")
   )
 
-  writeLines(preamble, here::here("inst/preamble.md"))
-
   return(list(
     dates = pub_dates_out,
+    reference_period = reference_period,
     preamble = preamble
   ))
 }
