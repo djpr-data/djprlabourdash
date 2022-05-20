b16794b7a14bfb6744df845772015f4eba7b7ede
Matt Johnson <mrjoh3@gmail.com>
2022-05-20 15:45:14 +1000

    remove ui elements with duplicated ID's. Looks to have been a copy paste error

--- R/page_age.R
+++ R/page_age.R
@@ -19,11 +19,8 @@ page_ageUI <- function(...) {
         djpr_plot_ui("gr_yth_lfpartrate_vicaus_line")
       )
     ),
-    uiOutput("table_gr_youth_summary") %>%
-      djpr_with_spinner() %>%
-      shinydashboard::box(width = 9),
-    djpr_plot_box("gr_yth_emp_sincecovid_line"),
-    djpr_plot_box("gr_yth_lfpartrate_vicaus_line"),
+
+
     focus_box(
       h2("Labour force status of Victorian youth"),
       shiny::selectInput("youth_focus",
@@ -60,15 +57,20 @@ page_ageUI <- function(...) {
                       width = 12)
       )
     ),
+
+
     djpr_h2_box("Detailed labour force status of Victorian youth"),
     djpr_plot_box("gr_youth_full_part_line"),
     djpr_plot_box("gr_youth_eduemp_waterfall",
       interactive = FALSE
     ),
     djpr_plot_box("gr_yth_mostvuln_line"),
+
+
     djpr_h2_box("Youth unemployment rate by region"),
-    uiOutput("table_gr_youth_unemp_region") %>%
-      djpr_with_spinner(),
+    uiOutput("table_gr_youth_unemp_region") %>% djpr_with_spinner(),
+
+
 
     focus_box(
       selectInput("youth_region_focus",
@@ -83,29 +85,32 @@ page_ageUI <- function(...) {
       uiOutput("title_youth_unemp_emppop_partrate_vic"),
       column(
         6,
-        leaflet::leafletOutput("map_youth_unemp_emppop_partrate_vic") %>%
-          djpr_with_spinner()
+        leaflet::leafletOutput("map_youth_unemp_emppop_partrate_vic") %>% djpr_with_spinner()
       ),
       column(
         6,
         plotOutput("gr_youth_unemp_emppop_partrate_bar")
       )
     ),
+
+
+
     djpr_h2_box("Jobactive caseload by age"),
-    uiOutput("table_jobactive_youth") %>%
-      djpr_with_spinner(),
+
+    uiOutput("table_jobactive_youth") %>% djpr_with_spinner(),
 
     djpr_plot_box("gr_youth_jobactive_bar"),
 
     djpr_plot_box("gr_age_jobactive_since_covid_line"),
 
-    uiOutput("table_jobactive_mature_age") %>%
-      djpr_with_spinner(),
+    uiOutput("table_jobactive_mature_age") %>% djpr_with_spinner(),
+
     djpr_plot_box("gr_mature_age_jobactive_bar")
+
   )
 }
 
-page_age <- function(input, output, session, plt_change = plt_change, series_latestdates = series_latestdates, footnote = footnote) {
+page_age <- function(input, output, session, plt_change, series_latestdates, footnote) {
   output$table_gr_youth_summary <- renderUI({
     table_gr_youth_summary() %>%
       flextable::htmltools_value()
