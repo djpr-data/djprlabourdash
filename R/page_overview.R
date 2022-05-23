8326f762283b25025128b7b2361090a201bab3ad
Henry McMillan <henry.mcmillan@ecodev.vic.gov.au>
2022-05-23 14:58:45 +1000

    Overview cleanup

--- R/page_overview.R
+++ R/page_overview.R
@@ -1,73 +1,41 @@
 page_overviewUI <- function(...) {
 
-  loading_content <- div(
-    id = "loading_page",
-    br(),
-    br(),
-    br(),
-    br(),
-    djprshiny::centred_row(h4(" Loading data, please wait..."))
-  )
+  shiny::tagList(
+    column(
+      4,
+      djprshiny::djpr_h2_box("DJPR Jobs Dashboard"),
 
-  main_content <- shiny::tagList(
+      shinydashboard::box(
+        width = 12,
+        "Some content"
+      )
+    ),
 
-    # Overview text and table
-    djprshiny::djpr_h2_box("DJPR Jobs Dashboard"),
+
     shinydashboard::box(
       title = shiny::h3("main table goes here"),
-      width = 12,
-      shiny::uiOutput("main_table", height = "800px")
+      width = 8,
+      shiny::uiOutput("main_table", height = "800px") %>%
+        djpr_with_spinner(proxy.height = "800px")
     ),
+
     shinydashboard::box(
       title = shiny::h4("footnote goes here"),
       width = 12,
       shiny::uiOutput("overview_footnote")
     )
+
   )
 
-  # main_content <- shinyjs::hidden(
-  #   div(
-  #     id = "main_content",
-  #     br(),
-  #     br(),
-  #     br(),
-  #     br(),
-  #     djprshiny::centred_row(
-  #       h1("DJPR Jobs Dashboard")
-  #     ),
-  #     br(),
-  #     djprshiny::centred_row(
-  #       uiOutput("main_table", height = "800px") %>%
-  #         djpr_with_spinner(hide.ui = TRUE)
-  #     ),
-  #     br(),
-  #     djprshiny::centred_row(htmlOutput("overview_footnote")),
-  #     br(),
-  #     br()
-  #   )
-  # )
-
-  tabPanel(
-    title = "Overview",
-    ggiraph_js(),
-    HTML(""),
-    value = "tab-overview",
-    shinyjs::useShinyjs(),
-    loading_content,
-    main_content
-  )
 }
 
 page_overview <- function(input, output, session, plt_change, series_latestdates, footnote) {
+
   output$main_table <- renderUI({
     req(dash_data)
     table_overview() %>%
       flextable::htmltools_value()
   }) %>%
     bindCache(series_latestdates)
-
-  observeEvent(input$link_overview, {
-    updateNavbarPage(session, "navbarpage", "tab-overview")
-  })
 
 }
