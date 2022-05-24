64814168c6878fc187db71991304c48ea1fc81ba
Emma Seyoum-Tegegn (DJPR) <emma.seyoum-tegegn@ecodev.vic.gov.au>
2022-05-24 11:37:53 +1000

    The UI for page_methodology has been created  and the labour_ui updatedto include the methodology page

---
+++ R/page_methodology.R
@@ -0,0 +1,20 @@
+page_methodologyUI <- function(...) {
+  djpr_tab_panel(
+    title = "Notes",
+    value = "tab-notes",
+    br(),
+    shiny::includeMarkdown("R/methodology.md"),
+    br(),
+
+    shiny::includeMarkdown("R/glossary.md"),
+
+    shiny::br(),
+
+    shiny::includeMarkdown("R/note.md"),
+
+    shiny::br(),
+    centred_row(htmlOutput("methodology_footnote")),
+    br()
+  )
+}
+
