362976c3d7f7b691e9c7fa91396c36a71314415f
Henry S McMillan (DJPR) <henry.mcmillan@ecodev.vic.gov.au>
2022-06-10 09:04:44 +1000

    djprshiny version check

--- R/labour_app.R
+++ R/labour_app.R
@@ -7,6 +7,14 @@
 #' @importFrom rlang .data .env
 
 app <- function(...) {
+
+  if(!exists("djpr_box_server", asNamespace("djprshiny"))){
+    stop(
+      "Please install the latest version of djprshiny with the following:\n",
+      "   remotes::install_github('djpr-data/djprshiny')"
+    )
+  }
+
   djprtheme::djpr_use_fonts()
   flextable::set_flextable_defaults(
     font.family = getOption("djprtheme.base_font_family")
